b4cb2d50da7331b99e83e0fb32dbf04fb5db749b
Fernando <fernando.martins@organicadigital.com>
2020-02-07 16:19:39 -0300

    Replaced %20 for " " in urls

--- src/utmkeeper.js
+++ src/utmkeeper.js
@@ -62,6 +62,9 @@ let utmkeeper = {};
 
         // set parameter value (use 'true' if empty)
         let paramValue = typeof a[1] ==='undefined' ? true : a[1];
+        if (typeof paramValue == "string"){
+        paramValue = paramValue.replace(/%20/g, " ");
+        }
 
         // set lowercase if camel case not set
         if(config.camelCase !== true) {
